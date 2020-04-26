185b28e63c93b0e6aca70d20b4bb24ad0cfa75e7
Adrian <adrian.rom64@gmail.com>
2020-04-26 17:10:14 +0200

    Add .env example

--- src/config.js
+++ src/config.js
@@ -1,19 +1,17 @@
 
 import { urlQuery } from './utils';
 
-const { env } = process;
-
 const getGithubUrl = () => {
   const baseUrl = 'https://github.com/login/oauth/authorize';
-  const params = { client_id: env.REACT_APP_GITHUB_CLIENT_ID };
+  const params = { client_id: process.env.REACT_APP_GITHUB_CLIENT_ID };
   return `${baseUrl}?${urlQuery(params)}`;
 };
 
 const getFacebookUrl = () => {
-  const url = `https://www.facebook.com/${env.REACT_APP_FACEBOOK_API_VERSION}/dialog/oauth`;
+  const url = `https://www.facebook.com/${process.env.REACT_APP_FACEBOOK_API_VERSION}/dialog/oauth`;
   const params = {
-    client_id: env.REACT_APP_FACEBOOK_APP_ID,
-    redirect_uri: `${env.REACT_APP_SITE_URL}/auth/facebook`,
+    client_id: process.env.REACT_APP_FACEBOOK_APP_ID,
+    redirect_uri: `${process.env.REACT_APP_SITE_URL}/auth/facebook`,
     state: 'placeholder',
   };
   return `${url}?${urlQuery(params)}`;
@@ -22,17 +20,17 @@ const getFacebookUrl = () => {
 const getSpotifyUrl = () => {
   const baseUrl = 'https://accounts.spotify.com/authorize';
   const params = {
-    client_id: env.REACT_APP_SPOTIFY_CLIENT_ID,
+    client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
     response_type: 'code',
-    redirect_uri: `${env.REACT_APP_SITE_URL}/auth/spotify`,
+    redirect_uri: `${process.env.REACT_APP_SITE_URL}/auth/spotify`,
     state: 'placeholder',
   };
   return `${baseUrl}?${urlQuery(params)}`;
 };
 
 export default {
-  siteUrl: env.REACT_APP_SITE_URL,
-  apiUrl: env.REACT_APP_API_URL,
+  siteUrl: process.env.REACT_APP_SITE_URL,
+  apiUrl: process.env.REACT_APP_API_URL,
   githubUrl: getGithubUrl(),
   facebookUrl: getFacebookUrl(),
   spotifyUrl: getSpotifyUrl(),
